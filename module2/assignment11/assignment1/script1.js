ffbeb9554590bd2f064201757a08bf4f429981f5
Timur Sindirinschi <timur.sindirinschi@metropolia.fi>
2024-11-21 23:19:47 +0200

    adjust module 2

--- module2/assignment11/assignment1/script1.js
+++
@@ -1,9 +0,0 @@
-let nums = [];
-for (let i = 0; i < 5; i++) {
-  nums.push(+prompt('Enter a number'));
-}
-
-console.log('Numbers in reverse: ');
-for (let i = nums.length - 1; i >= 0; i--) {
-  console.log(nums[i]);
-}

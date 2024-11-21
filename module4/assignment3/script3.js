2a982aeab528261a77f63cd28d54b0eb9dc30a9b
Timur Sindirinschi <timur.sindirinschi@metropolia.fi>
2024-11-21 23:17:16 +0200

    Delete module4 directory

--- module4/assignment3/script3.js
+++
@@ -1,9 +0,0 @@
-let dogs = [];
-for (let i = 0; i < 6; i++) {
-    let dog = prompt('Enter the name of the dog');
-    dogs.push(dog);
-}
-dogs.sort().reverse();
-document.write('<ul>');
-dogs.forEach(dog => document.write(`<li>${dog}</li>`));
-document.write("</ul>");
